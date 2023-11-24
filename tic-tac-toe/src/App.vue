e60d2579006f65bac2a2d5c04bf3c63e5c0fbfcd
GregoryKogan <grisha.koganovskiy@gmail.com>
2023-11-24 19:28:25 +0300

    Vue app initialized

---
+++ tic-tac-toe/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <router-view />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { useAppStore } from "@/store/app";
+
+export default defineComponent({
+  name: "App",
+  setup() {
+    const store = useAppStore();
+    return { store };
+  },
+});
+</script>
+
+<style>
+body {
+  background-color: rgb(var(--v-theme-background));
+}
+</style>
---
+++ tic-tac-toe/src/router/index.ts
@@ -0,0 +1,23 @@
+// Composables
+import { createRouter, createWebHistory } from 'vue-router';
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('@/layouts/default/Default.vue'),
+    children: [
+      {
+        path: '',
+        name: 'Home',
+        component: () => import('@/views/Home.vue'),
+      },
+    ],
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+});
+
+export default router;
