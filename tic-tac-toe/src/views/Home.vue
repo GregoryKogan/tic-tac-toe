c38fc69ae0ab54e9a8618f268afb222ff24c1568
GregoryKogan <grisha.koganovskiy@gmail.com>
2023-11-26 17:50:35 +0300

    Add wasm functionality

--- tic-tac-toe/src/views/Home.vue
+++ tic-tac-toe/src/views/Home.vue
@@ -1,3 +1,29 @@
 <template>
-  <h1>HelloWorld</h1>
+  <v-container>
+    <h1>HelloWorld</h1>
+    <v-btn @click="run">RUN</v-btn>
+  </v-container>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { int_sqrt } from "@/wasm/functions";
+
+export default defineComponent({
+  name: "Home",
+  methods: {
+    run() {
+      console.log(int_sqrt(1000000000));
+
+      const wasm_worker = new Worker(
+        new URL("@/wasm/very_slow_function.worker.ts", import.meta.url)
+      );
+      wasm_worker.onmessage = (e) => {
+        console.log(e.data);
+      };
+      wasm_worker.postMessage(10);
+    },
+  },
+});
+</script>
+@/wasm/wasm_functions
